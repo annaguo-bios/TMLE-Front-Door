106a85ff29a5f301d40b33f6f1cf14b0c374b113
annaguo-bios <anna.guo@emory.edu>
2025-06-09 15:54:57 -0400

    renamed data application folder

---
+++ real-data-application/B_PROUD/write_job.R
@@ -0,0 +1,8 @@
+joblist <- c()
+for (m in 1:10){
+
+  job <- paste0("Rscript main.R ",m)
+  joblist <- c(joblist,job)
+
+  write.table(joblist, file = paste0("joblist_n",1,".txt"),quote = F, col.names = F, row.names = F)
+}
